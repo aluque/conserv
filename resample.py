3387a074a5754b1774c8ca76b747f3618764c335
Alejandro Luque Estepa <aluque@iaa.es>
2023-02-21 23:17:20 +0100

    Add resample.py.

---
+++ resample.py
@@ -0,0 +1,44 @@
+import os
+import glob
+
+import h5py
+import numpy as np
+
+def main(path=os.path.expanduser("~/data/denoise/charge_density")):
+    for subset in ["original", "noisy_50"]:
+        resample(path, subset, 16)
+
+def resample(path, subset, step):
+    rpath = os.path.join(path, "x%d" % step, subset)
+    
+    os.makedirs(rpath, exist_ok=True)
+    
+    for pfname in glob.iglob(os.path.join(path, subset, "*.hdf")):
+        print(pfname)
+
+        _, fname = os.path.split(pfname)
+        rfname = os.path.join(rpath, fname)
+        
+        q = loadfile(pfname)
+        savefile(rfname, q[::step, ::step])
+
+
+def loadfile(fname):
+    with h5py.File(fname, mode='r') as file:
+        patch = list(file.keys())[0]        # name of the first group contained in the file
+    
+        group = file[patch]                 # first group
+        dataset = list(group.keys())[0]     # name of the first dataset in the group (c-density)
+        q = np.array(group[dataset]).astype('float32', copy = False)
+
+    return q
+
+
+def savefile(fname, d):
+    with h5py.File(fname, mode='w') as file:  # loading the hdf5 file
+        g = file.create_group("group1")
+        dset = g.create_dataset("charge_density", data=d, compression="lzf",
+                                dtype='float32')
+    
+if __name__ == '__main__':
+    main()
