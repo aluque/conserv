cd34c3d4a66a929ffca261034dae24dd456e9061
Alejandro Luque <aluque@iaa.es>
2023-02-24 11:47:19 +0100

    Improve showkernels.py.

--- showkernels.py
+++ showkernels.py
@@ -4,15 +4,22 @@ import numpy as np
 import tensorflow as tf
 from matplotlib import pyplot as plt
 
+try:
+    plt.style.use("granada")
+except OSError:
+    pass
+
 from train import load2
 from model import CUSTOM_OBJECTS
 
 def main():
+    
     model = tf.keras.models.load_model('checkpoint.hdf5',
                                        custom_objects=CUSTOM_OBJECTS)
     K = model.get_layer("K").get_weights()[0]
     
     plt.figure("kernels", figsize=(20, 10))
+    plt.suptitle("Kernels $K^{(n)}$", size=16)
     for i in range(32):
         ax = plt.subplot(4, 8, i + 1)
         ax.matshow(K[:, :, i])
@@ -28,15 +35,26 @@ def main():
     c = probing_layer.predict(q1)
 
     plt.figure("weights", figsize=(20, 10))
-    for i in range(32):
-        ax = plt.subplot(4, 8, i + 1)
-        ax.pcolormesh(c[0, :, :, i], vmin=0, vmax=1, cmap="gnuplot2")
-        ticks = ax.get_xticks()
-        ax.set_xticklabels(["" for t in ticks])
+    plt.suptitle("Partition coeffs $c^{(n)}$", size=16)
+    for i in range(4):
+        for j in range(8):
+            n = i * 8 + j
+            ax = plt.subplot(4, 8, n + 1)
+            ax.pcolormesh(c[0, :, :, n], vmin=0, vmax=1, cmap="gnuplot2")
+            
+            if i != 3:
+                ticks = ax.get_xticks()
+                ax.set_xticklabels(["" for t in ticks])
+            else:
+                ax.set_xlabel("r (px)")
+                
+            if j != 0:
+                ticks = ax.get_yticks()
+                ax.set_yticklabels(["" for t in ticks])
 
-        ticks = ax.get_yticks()
-        ax.set_yticklabels(["" for t in ticks])
-
+            else:
+                ax.set_ylabel("z (px)")
+                
     plt.show()
     
 if __name__ == '__main__':
